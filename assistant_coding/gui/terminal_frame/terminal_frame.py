b481b4a6cffaa3536e21ae4d5bd99eef8fccc19c
komikodok <komikodok666@gmail.com>
2024-10-15 20:07:05 +0700

    edited terminal_frame.py

--- assistant_coding/gui/terminal_frame/terminal_frame.py
+++ assistant_coding/gui/terminal_frame/terminal_frame.py
@@ -4,7 +4,7 @@ import subprocess
 import os
 
 from threading import Thread
-from langchain_core.messages import HumanMessage, AIMessage
+import re
 from assistant_coding.llms.llm_app import LLMApp
 
 
@@ -38,29 +38,26 @@ class TerminalFrame:
         self.text.mark_set("insert", "end-1c")
         self.text.see(tk.END)
 
+    def threading_llm(self, user_input):
+        # Freeze tombol dan entry ketika proses threading llm_invoke
+        self.chatbot_frame.freeze_button_entry_while_threading() # Method dari class ChatbotFrame
+        self.text.bind('<Return>', self.chatbot_frame.pass_process())
+
+        llm_response = self.llm_app.invoke(
+            question=user_input, 
+            chat_history=self.chatbot_frame.chat_history, # Atribute dari class ChatbotFrame
+            is_error_message=True
+        )
+        response_items = llm_response.get_response_items()
+        # Menampilkan response bot ke dalam chatbot
+        self.chatbot_frame.add_message(f"Bot: {response_items['generation']}") # Method dari class ChatbotFrame
+        self.chatbot_frame.loading_frame.grid_forget() # Method dari class ChatbotFrame
+
+        # Mengembalikan tombol dan entry ke dalam keadaan semula setelah proses threading_llm
+        self.chatbot_frame.return_button_entry_after_threading() # Method dari class ChatbotFrame
+        self.text.bind('<Return>', self.execute_command)
+
     def execute_command(self, event=None):
-        def threading_llm(user_input):
-            # Freeze tombol dan entry ketika proses threading llm_invoke
-            self.chatbot_frame.freeze_button_entry_while_threading() # Method dari class ChatbotFrame
-            self.text.bind('<Return>', self.chatbot_frame.pass_process())
-
-            llm_response = self.llm_app.invoke(
-                question=user_input, 
-                chat_history=self.chatbot_frame.chat_history, # Atribute dari class ChatbotFrame
-                from_terminal=True
-            )
-            response_items = llm_response.get_response_items()
-            # Menampilkan response bot ke dalam chatbot
-            self.chatbot_frame.add_message(f"Bot: {response_items['generation']}") # Method dari class ChatbotFrame
-            # Tambahkan ke chat history
-            self.chatbot_frame.chat_history.append(HumanMessage(content=response_items['question'])) # Atribute dari class ChatbotFrame
-            self.chatbot_frame.chat_history.append(AIMessage(content=response_items['generation'])) # Atribute dari class ChatbotFrame
-
-            self.chatbot_frame.loading_frame.grid_forget() # Method dari class ChatbotFrame
-
-            # Mengembalikan tombol dan entry ke dalam keadaan semula setelah proses threading_llm
-            self.chatbot_frame.return_button_entry_after_threading() # Method dari class ChatbotFrame
-            self.text.bind('<Return>', self.execute_command)
 
         # Mendapatkan perintah yang dimasukkan oleh pengguna di terminal
         command = self.get_input_command()
@@ -69,26 +66,7 @@ class TerminalFrame:
             self.text.insert(tk.END, "\n")
             self.text.see(tk.END)
 
-            # Menjalankan perintah
-            terminal_run = subprocess.run(command, shell=True, text=True, capture_output=True)
-
-            output = terminal_run.stdout if terminal_run.stdout else terminal_run.stderr
-
-            if output == terminal_run.stdout:
-                # Menampilkan output di terminal
-                self.text.insert(tk.END, output)
-            else:                   
-                # Menampilkan output error di terminal
-                self.text.insert(tk.END, output)
-
-                user_input = f"cara mengatasi {output}"
-                # Menampilkan error dari terminal ke dalam chatbot
-                self.chatbot_frame.add_message(f"You: {user_input}") # Method dari class ChatbotFrame
-
-                self.chatbot_frame.loading_frame.grid(column=0, row=0) # Atribute dari class ChatbotFrame
-                self.chatbot_frame.loading_screen() # Method dari class ChatbotFrame
-                Thread(target=threading_llm, args=(user_input, )).start()
-
+        self.get_output_command(command)
 
         # Pindah ke baris baru dan menampilkan prompt kembali
         self.text.insert(tk.END, "\n")
@@ -99,3 +77,31 @@ class TerminalFrame:
         # Mendapatkan teks yang diinput dari prompt terakhir hingga akhir teks
         command = self.text.get("end-1c linestart", "end-1c")
         return command.split("$ ", 1)[-1].strip()
+    
+    def get_output_command(self, command):
+        try:
+            # Menjalankan perintah
+            terminal_run = subprocess.run(command, shell=True, text=True, capture_output=True, check=True)
+            
+            # Menampilkan output di terminal jika tidak terjadi error
+            self.text.insert(tk.END, terminal_run.stdout)
+        except subprocess.CalledProcessError as err:
+            error_message = err.stderr
+            # Menampilkan output error di terminal jika terjadi error
+            self.text.insert(tk.END, error_message)
+
+            self.chatbot_frame.loading_frame.grid(column=0, row=0) # Atribute dari class ChatbotFrame
+            self.chatbot_frame.loading_screen() # Method dari class ChatbotFrame
+
+            # Menggunakan regex untuk menangkap kode program berdasarkan path dari traceback
+            traceback_patter = r'File "(?P<filename>.*?)".*?\n\s*(?P<code>.*?)\n'
+            matches = re.finditer(traceback_patter, error_message)
+            list_matches = list(matches)
+            with open(list_matches[0].group("filename"), "r") as f:
+                script_code = f.read()
+                print(script_code)
+
+            user_input = f"Cara mengatasi error: \n{error_message} \nberdasarkan kode berikut: \n{script_code}"
+            # Menampilkan error dari terminal ke dalam chatbot
+            self.chatbot_frame.add_message(f"You: {error_message}") # Method dari class ChatbotFrame
+            Thread(target=self.threading_llm, args=(user_input, )).start()
